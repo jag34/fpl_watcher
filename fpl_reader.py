b4e3fce5a0d6762b7f46a18747d430c2642c181d
jag34 <jag34@users.noreply.github.com>
2017-07-13 17:54:43 -0400

    Initial commit for hourly usage
    Refactored a few things as well.

--- fpl_reader.py
+++ fpl_reader.py
@@ -1,5 +1,42 @@
-import requests
-from response_parser import parse_response
+# Needed for the query to work:
+# zipCode
+# accountNumber: Not being there, affects the estimated cost. Different account #s return different approx cost values.
+# startDate, endDate; for the query
+# lastAvailableDate: Needs to be there, but doesn't actually need a valid value.
+# certifiedDate: Doesn't need to be accurate, just needs to be there
+
+# Everything else should be kept the same over other requests.
+# A lot of these values might be necessary for providing echo's back for the front
+# end library to properly fill out fields in the page.
+
+# TODO: There is a bug where the first day on the query is really large.
+
+base_query = { 'premiseNumber' : '',
+'startDate' : '',
+'endDate' : '',
+'accountNumber' : '',
+'accountType' : 'null',
+'zipCode' : '',
+'consumption' : '0.0',
+'usage' : '0.0',
+'isMultiMeter' : 'false',
+'lastAvailableDate' : '1970/01/01',
+'isAmiMeter' : 'true',
+'userType' : 'EXT',
+'currentReading' : '',
+'isResidential' : 'true',
+'isTouUser' : 'false',
+'showGroupData' : 'false',
+'isNetMeter' : 'false',
+'certifiedDate' : '1970/01/01',
+'userId' : '',
+'acctNetMeter' : 'false',
+'tempType' : 'max',
+'viewType' : 'dollar',
+'ecDayHumType' : 'NoHum',
+'ecHasMoveInRate' : 'true',
+'ecMoveInRateVal' : '',
+'lastAvailableIeeDate' : '20170705'}
 
 query_params = { 'premiseNumber' : '183732653',
 'startDate' : '20170623',
@@ -19,7 +56,7 @@ query_params = { 'premiseNumber' : '183732653',
 'showGroupData' : 'false',
 'isNetMeter' : 'false',
 'certifiedDate' : '2017/06/23',
-'userId' : 'jaguilar@aguilarstiller.com',
+'userId' : '',
 'acctNetMeter' : 'false',
 'tempType' : 'max',
 'viewType' : 'dollar',
@@ -29,8 +66,8 @@ query_params = { 'premiseNumber' : '183732653',
 'lastAvailableIeeDate' : '20170705'}
 
 query_params_2 = { 'premiseNumber':'743019753',
-'startDate':'20170417',
-'endDate':'20170518',
+'startDate':'20170101',
+'endDate':'20170201',
 'isAmiMeter':'true',
 'accountNumber':'5720511046',
 'accountType':'residential',
@@ -38,11 +75,11 @@ query_params_2 = { 'premiseNumber':'743019753',
 'consumption':'0.0',
 'usage':'0.0',
 'isMultiMeter':'false',
-'lastAvailableDate':'2017/07/05',
+'lastAvailableDate':'2017/06/23',
 'userType':'EXT',
-'currentReading':'43031',
+'currentReading':'',
 'certifiedDate':'2014/05/31',
-'userId':'jaguilar@aguilarstiller.com',
+'userId':'',
 'isResidential':'true',
 'acctNetMeter':'false',
 'tempType':'max',
@@ -50,27 +87,64 @@ query_params_2 = { 'premiseNumber':'743019753',
 'ecDayHumType':'NoHum',
 'ecHasMoveInRate':'false',
 'ecMoveInRateVal':'',
-'lastAvailableIeeDate':'20170706' }
+'lastAvailableIeeDate':'20170623' }
 
-http_proxy  = "http://127.0.0.1:3128"
-https_proxy = "https://127.0.0.1:3128"
+DAILY_CONSUMPTION_URL = 'https://app.fpl.com/wps/PA_ESFPortalWeb/getDailyConsumption.do'
+HOURLY_CONSUMPTION_URL = 'https://app.fpl.com/wps/PA_ESFPortalWeb/getHourlyConsumption.do'
+START_END_DATE_FORMAT = '%Y%m%d'
 
+# only for use when there's a corporate proxy
 proxyDict = {
-              "http"  : http_proxy,
-              "https" : https_proxy,
+              "http"  : "http://127.0.0.1:3128",
+              "https" : "https://127.0.0.1:3128",
             }
 
-import requests
 
-def make_fpl_request():
-    r = requests.get(url='https://app.fpl.com/wps/PA_ESFPortalWeb/getDailyConsumption.do',
-                     proxies = proxyDict,
-                     params=query_params_2,
-                     verify=False)
+def validate_date(date, expected):
+    from datetime import datetime
+    return datetime.strptime(date, expected)
+
+
+def make_http_request(**kwargs):
+    import requests
+    print kwargs['params']
+    r = requests.get(**kwargs)
     return r.content
 
+
+def request_daily_usage(premise_no, start_date, end_date, zip_code, acct_no='', proxy=None):
+    from response_parser import parse_daily_usage
+    #from datetime import datetime
+    #start_date_datetime = datetime.strptime(start_date, START_END_DATE_FORMAT)
+    #end_date_datetime = datetime.strptime(end_date, START_END_DATE_FORMAT)
+    current_query = base_query
+
+    current_query['premiseNumber'] = premise_no
+    current_query['startDate'] = start_date
+    current_query['endDate'] = end_date
+    current_query['zipCode'] = zip_code
+    current_query['accountNumber'] = acct_no
+
+    request_dict = {'url': DAILY_CONSUMPTION_URL,
+                    'params' : current_query}
+
+    if proxy:
+        request_dict['proxies'] = proxy
+        request_dict['verify'] = False
+
+    response = make_http_request(**request_dict)
+    data_range = parse_daily_usage(response)
+
+    return data_range
+
+
 if '__main__' == __name__:
-    print make_fpl_request()
-    data_range = parse_response(make_fpl_request())
+    data_range = request_daily_usage(premise_no='743019753',
+                                     start_date='20170101',
+                                     end_date='20170201',
+                                     zip_code='32904',
+                                     acct_no='5720511046',
+                                     proxy=proxyDict)
+
     for data_point in data_range:
         print data_point
